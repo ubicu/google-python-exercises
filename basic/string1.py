23bacfd4ed495414d0e0f415bdc6e74ebcd2d496
= <=>
2016-08-11 18:56:13 -0400

    Modified mix_up method in basic/string1.py

--- basic/string1.py
+++ basic/string1.py
@@ -72,7 +72,7 @@ def fix_start(s):
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
   # +++your code here+++
-  return
+  return b[0:2]+a[2:]+' '+a[0:2]+b[2:] 
 
 
 # Provided simple test() function used in main() to print
