e2d6900ae7d1fc102879fb71239741085727e720
= <=>
2016-08-12 15:15:47 -0400

    Modified linear_merge method in basic/list2.py

--- basic/list2.py
+++ basic/list2.py
@@ -41,7 +41,7 @@ def remove_adjacent(nums):
 # pass of both lists.
 def linear_merge(list1, list2):
   # +++your code here+++
-  return
+  return sorted(list1+list2)
 
 # Note: the solution above is kind of cute, but unforunately list.pop(0)
 # is not constant time with the standard python list implementation, so
