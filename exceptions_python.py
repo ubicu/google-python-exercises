1376b69df85d95327efc887c5879d3c4cc36c73b
Ham Rara <hmrara01@gmail.com>
2016-08-17 15:02:07 -0400

    Initial code for exceptions_python.py

---
+++ exceptions_python.py
@@ -0,0 +1,19 @@
+ # Exceptions example from lecture
+ 
+import sys
+ 
+def Cat(filename):
+  with open(filename, 'r') as f:
+    text = f.read()
+    print('---'+filename)
+    print(text)
+ 
+def main():
+  args = sys.argv[1:]
+  for arg in args:
+    Cat(arg)
+ 
+ 
+ # This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main()
