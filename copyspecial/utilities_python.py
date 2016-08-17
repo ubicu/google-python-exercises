96a16c934985ba3b876e633544e27c1560a7f54a
Ham Rara <hmrara01@gmail.com>
2016-08-17 09:19:21 -0400

    Added code to list files in a directory in utilities_python.py

--- copyspecial/utilities_python.py
+++ copyspecial/utilities_python.py
@@ -1,3 +1,25 @@
 
 # Exercises from the Utilities lecture
 
+import sys
+import os
+
+def List(dir):
+  filenames = os.listdir(dir)
+  print filenames
+
+def main():
+  
+  args = sys.argv[1:]
+
+  if not args:
+    print 'usage: utilities_python.py directory_name'
+    sys.exit(1)
+	
+  # Test code
+  List(args[0])
+
+
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main()
