653b5ac75053fe52947859ce712ddc0e4107c999
Ham Rara <hmrara01@gmail.com>
2016-08-18 00:45:08 -0400

    Added urllib.urlretrieve example

--- logpuzzle/more_utilities_python.py
+++ logpuzzle/more_utilities_python.py
@@ -3,4 +3,6 @@
 import urllib
 
 uf = urllib.urlopen('http://www.google.com')
-print(uf.read())
+# print(uf.read())
+
+urllib.urlretrieve('http://www.google.com/intl/en_ALL/images/logo.gif','google.gif')
